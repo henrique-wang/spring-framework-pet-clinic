346376004e27a90f46e3690f83f855fa710c2070
Henrique Igai Wang <64105985+henrique-wang@users.noreply.github.com>
2020-10-02 14:24:00 -0300

    Implemented Map Based Services. Closes #4 Added CrudService as Common Base Interface for all services. Closes #11

--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/PetInterface.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/PetInterface.java
@@ -2,10 +2,5 @@ package springframework.sfgpetclinic.services;
 
 import springframework.sfgpetclinic.models.Pet;
 
-import java.util.Set;
-
-public interface PetInterface {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetInterface extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package springframework.sfgpetclinic.services;
 
 import springframework.sfgpetclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
